d2cd95a257c332830120b98d3813cccbc133eba8
Boryana Dimitrova <b.dimitrova97@gmail.com>
2024-07-23 11:13:23 +0300

    Authentication

--- src/models/Devices.js
+++ src/models/Devices.js
@@ -4,52 +4,67 @@ let devicesSchema = new mongoose.Schema({
     brand: {
         type: String,
         require: true,
+        minLength: 2
     },
     model: {
         type: String,
         require: true,
+        minLength: 5
     },
     hardDisk: {
         type: String,
         require: true,
+        minLength: 5
     },
     screenSize: {
         type: String,
         require: true,
+        minLength: 1
     },
     ram: {
         type: String,
         require: true,
+        minLength: 2
     },
     operatingSystem: {
         type: String,
         require: true,
+        minLength: 5,
+        maxLength: 20
     },
     cpu: {
         type: String,
         require: true,
+        minLength: 10,
+        maxLength: 50
     },
     gpu: {
         type: String,
         require: true,
+        minLength: 10,
+        maxLength: 50
     },
     price: {
         type: Number,
         require: true,
+        minValue: 0
     },
     color: {
         type: String,
         require: true,
+        minLength: 2,
+        maxLength: 10
     },
     weight: {
         type: String,
         require: true,
+        minLength: 1
     },
     image: {
         type: String,
         require: true,
         validate: /^https?:\/\//i,
-        
+
     },
     preferedList: [
         {
